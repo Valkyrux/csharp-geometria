ed266c1daf79d7cfe41c2ac93c6f6be9df940a72
Phil.adelphos <phil.adelphos.dev@gmail.com>
2022-05-12 17:44:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace chsharp_geometria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci la base del rettangolo");
+            int inputBase = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserisci l'altezza del rettangolo");
+            int inputAltezza = int.Parse(Console.ReadLine());
+            Rettangolo primoRettangolo = new Rettangolo(inputBase, inputAltezza);
+            Console.WriteLine("Il perimetro misura: {0}", primoRettangolo.calcolaPerimetro());
+            Console.WriteLine("L'area misura: {0}", primoRettangolo.calcolaArea());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\n--Rettangolo-{0}--", i);
+                Rettangolo unRettangolo = new Rettangolo(new Random().Next(1, 20), new Random().Next(1, 20));
+                unRettangolo.stampaRettangolo();
+            }
+
+            int baseDisegno = 5;
+            int altezzaDisegno = 8;
+
+
+            Rettangolo secondoRettangolo = new Rettangolo(baseDisegno, altezzaDisegno);
+            Console.WriteLine("\nTriangolo disegnato base: {0}, altezza: {1}", baseDisegno, altezzaDisegno);
+            secondoRettangolo.disegnaRettangolo();
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,64 @@
+﻿namespace chsharp_geometria
+{
+    internal class Rettangolo
+    {
+        private int baseRettangolo;
+        private int altezzaRettangolo;
+
+        public Rettangolo(int inputB, int inputA)
+        {
+            this.baseRettangolo = inputB;
+            this.altezzaRettangolo = inputA;
+        }
+
+        public int calcolaArea()
+        {
+            return this.baseRettangolo * this.altezzaRettangolo;
+        }
+
+        public int calcolaPerimetro()
+        {
+            return 2 * this.baseRettangolo + 2*this.altezzaRettangolo;
+        }
+
+        public void stampaRettangolo()
+        {
+            Console.WriteLine("base: {0}, altezza: {1}, area: {2}, perimetro: {3}", this.baseRettangolo, this.altezzaRettangolo, this.calcolaArea(), this.calcolaPerimetro());
+        }
+
+        public void disegnaRettangolo()
+        {
+            string baseDisegnata = "";
+            string altezzaDisegnata = "";
+            for (int i = 0; i < this.baseRettangolo; i++)
+            {
+                baseDisegnata += "-";
+                if(i == 0 || i == this.baseRettangolo -1)
+                {
+                    altezzaDisegnata += "|";
+                } else
+                {
+                    altezzaDisegnata += " ";
+                }
+            }
+
+            for (int i = 0; i < this.altezzaRettangolo; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(baseDisegnata);
+                    Console.WriteLine(altezzaDisegnata);
+                }
+                else if(i == this.altezzaRettangolo - 1)
+                {
+                    Console.WriteLine(altezzaDisegnata);
+                    Console.WriteLine(baseDisegnata);
+                }
+                else
+                {
+                    Console.WriteLine(altezzaDisegnata);
+                }
+            }
+        }
+    }
+}
